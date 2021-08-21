d3fbffea24706c47433d7f6cc1ddd7b1a23680d5
Amank-root <70597017+Amank-root@users.noreply.github.com>
2021-08-21 20:26:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ taskbar.addEventListener("click", () => {
         startmenu.style.bottom = "50px"
     }
 })
-if (screen.width >= 1200) { 
+if (screen.width >= 1201) { 
     let somt = alert("You can Provide Your Details Here Without Any Hesitation Because This Site Is Static. \n In Short Does not Save Any Data.");
     let provided = prompt("Enter Your name Here.");
     let namee = document.getElementById("namee");
